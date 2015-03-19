ffc050e15b5b617736f870efafeb61251acf5549
Joshua Bagwell <j00w33@gmail.com>
2015-03-19 08:36:26 -0400

    Started making a plan in the Maze class.

--- FinalMazeGit/src/maze/logic/Maze.java
+++ FinalMazeGit/src/maze/logic/Maze.java
@@ -2,16 +2,14 @@ package maze.logic;
 
 public class Maze {
 
-	/* Andrew */
-
-	//stuff
-	// code
-	// code
-	// code
-	/* Andrew end */
-
 	public Maze() {
 		// TODO Auto-generated constructor stub
 	}
+	/* Josh Start */
+	// method to turn mazeparser into a 2d array
+	// method to properly stack 2d array
+	// maybe have 2d array of chars, maybe not?
+	
+	/*Josh End*/
 
 }
