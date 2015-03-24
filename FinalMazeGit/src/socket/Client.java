8dbfc8de6ddde59ca13896cde2e7a86a04b4607c
Andrew <Andrew@10.99.5.231>
2015-03-24 11:43:00 -0400

    added new classes

---
+++ FinalMazeGit/src/socket/Client.java
@@ -0,0 +1,31 @@
+package socket;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import maze.logic.LogFile;
+
+/**
+ * 
+ * @author Andrew
+ *Client class for project
+ */
+public class Client {
+	public Client(String URL) {
+
+		
+		String serverName = "127.0.0.1";
+		int port = 13000;
+		try {
+			LogFile.write("Connecting to " + serverName + " on port "
+					+ port);
+			Socket client = new Socket(serverName, port);
+			LogFile.write("Just connected to "
+					+ client.getRemoteSocketAddress());
+			client.close();
+		} catch (IOException e) {
+			LogFile.write("IOException in Client, server not found");
+		}
+	}
+
+}
